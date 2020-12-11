0a213c268fce7cd46cb09dc8facd6de9770bc9ba
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-11 20:58:09 +0630

    basics routing, status code, header and custom-header

---
+++ node-firm/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+
+});
+
+server.listen(8000, '127.0.0.1', () => console.log('listening on port 8000'));
+
