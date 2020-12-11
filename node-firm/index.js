fb186a73bb3f4cafa59a45b4a878d03b430b6f99
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-11 19:37:36 +0630

    if there is no file error, error will be log in console

--- node-firm/index.js
+++ node-firm/index.js
@@ -8,7 +8,9 @@ const fs = require('fs');
 // console.log('File written!');
 
 // Non-blocking, asynchronous way 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+fs.readFile('./txt/starttt.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log(`ERROR ==> ${err}`);  // if there is no file, there will be error
+
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
         console.log(data2);
         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
