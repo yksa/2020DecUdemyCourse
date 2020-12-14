19e8b0b184a7d6ca02682a1c053c8645c50fd6e4
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-14 15:12:11 +0630

    using 3rd party modules

--- node-firm/final/node-firm-server.js
+++ node-firm/final/node-firm-server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -9,6 +11,9 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
