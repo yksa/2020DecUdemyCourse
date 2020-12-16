7ad0c47263ec09e7976b6a555f3a36a655810300
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-16 23:12:14 +0630

    Handling Delete Request

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -81,6 +81,22 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 app.get('/', (req, res) => {
   res
     .status(200)
