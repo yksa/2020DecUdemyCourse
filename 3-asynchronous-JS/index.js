206b8a8ec1ef2f68ba5ff9422b2d8b867051038c
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-16 12:54:53 +0630

    From Callback Hell to Promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,18 +1,34 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// callback 
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//     console.log(`Breed: ${data}`);
+
+//     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
+//         if (err) return console.log(err.message);
+
+//         console.log(res.body.message);
+
+//         fs.writeFile('dog-img.txt', res.body.message, err => {
+//             if (err) return console.log(err.message);
+
+//             console.log('Random dog image saved to file!');
+//         });
+//     });
+// });
+
+// promise 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-        if (err) return console.log(err.message);
-
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then( res => {
         console.log(res.body.message);
 
         fs.writeFile('dog-img.txt', res.body.message, err => {
             if (err) return console.log(err.message);
-            
-            console.log('Random dog image saved to file!');
+
+            console.log('Random dog image save to file!');
         });
-    });
+    }).catch( err => console.log(err));
 });
