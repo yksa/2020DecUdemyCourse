9513027633e9d2e1ef49ff47f35c1a73af2f660f
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-16 23:04:32 +0630

    Handling Patch Request

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -63,6 +63,24 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 app.get('/', (req, res) => {
   res
     .status(200)
