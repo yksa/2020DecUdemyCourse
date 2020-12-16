b63973178bb915676a2943548b34754f447dbbee
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-16 15:38:22 +0630

    Building Promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const { resolve } = require('path');
 const superagent = require('superagent');
 
-// callback 
+// callback
+
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 //     console.log(`Breed: ${data}`);
 
@@ -18,17 +20,36 @@ const superagent = require('superagent');
 //     });
 // });
 
-// promise 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`Breed: ${data}`);
+// promise
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then( res => {
-        console.log(res.body.message);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file');
+      resolve(data);
+    });
+  });
+};
 
-        fs.writeFile('dog-img.txt', res.body.message, err => {
-            if (err) return console.log(err.message);
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Error occurs');
+      resolve('Random dog image save to file!');
+    });
+  });
+};
 
-            console.log('Random dog image save to file!');
-        });
-    }).catch( err => console.log(err));
-});
+readFilePro(`${__dirname}/dog.txt`)
+  .then((res) => {
+    console.log(`Breed: ${res}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${res}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err.message));
