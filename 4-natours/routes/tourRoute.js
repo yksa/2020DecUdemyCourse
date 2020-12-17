73c0686fe290203c1e5b08386aea380f7336c679
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-17 15:36:31 +0630

    Chanining Multiple Middleware Functions

--- 4-natours/routes/tourRoute.js
+++ 4-natours/routes/tourRoute.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const {
-  getAllTours,
-  createTour,
-  getTour,
-  updateTour,
-  deleteTour,
-  checkId,
-} = require('./../controllers/tourController');
+const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', checkId);
+router.param('id', tourController.checkId);
 
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
