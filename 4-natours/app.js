a3f8aff33faef5b5a124f0750dbbc2981237348f
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-17 16:55:35 +0630

    Environment Variables

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,8 +7,9 @@ const userRouter = require('./routes/userRoute');
 const app = express();
 
 // 1) MIDDLEWARE
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.use((req, res, next) => {
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 3000;
+console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
