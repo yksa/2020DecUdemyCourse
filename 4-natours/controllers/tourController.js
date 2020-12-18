5def8c30f81a6db503a79a68d44f2003ea9d45e9
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-18 23:13:44 +0630

    Another way of Creating Documents

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -34,7 +24,23 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {};
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
