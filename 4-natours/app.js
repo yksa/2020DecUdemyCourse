3b7d3c8a287be8b4551757aa28e17eb2f6da899d
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-19 11:39:30 +0630

    Reading Documents

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -13,11 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
