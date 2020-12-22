ced783640098eed90ba43950ab307603021b9389
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-22 23:12:17 +0630

    Logging in Users

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -23,6 +23,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'User must have password.'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -47,6 +48,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
