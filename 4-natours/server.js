84d93861fcb58a43543d3ce952f3d6783707465a
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-22 13:16:41 +0630

    Catching Uncaught Exceptions

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -22,8 +28,8 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => console.log(`listening on port ${port}`));
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLER REJECTION! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
