5399c2068d3fcb3bf46b61df02f6bd1a161ae403
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-28 00:25:05 +0630

    Factory Functions: Reading

--- 4-natours/controllers/handlerFactory.js
+++ 4-natours/controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -38,6 +39,47 @@ exports.createOne = (Model) =>
       status: 'success',
       data: {
         doc: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested reviews on tour
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    const features = new APIFeatures(Model, req.query)
+      .filter(filter)
+      .sort()
+      .limitFields()
+      .paginate();
+    const docs = await features.query;
+
+    // Send Response
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        docs,
       },
     });
   });
--- 4-natours/controllers/reviewController.js
+++ 4-natours/controllers/reviewController.js
@@ -1,22 +1,5 @@
 const Review = require('../models/reviewModel');
-const APIFeatures = require('../utils/apiFeatures');
-const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
 
 exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
@@ -26,6 +9,8 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
+exports.getAllReviews = factory.getAll(Review);
 exports.createReview = factory.createOne(Review);
+exports.getReview = factory.getOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
