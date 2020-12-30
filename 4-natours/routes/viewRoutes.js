ff2fea3cf030e70f85b51cec47abeb375a578e1a
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-30 17:54:44 +0630

    Logging in Users with Our API - Part 2

--- 4-natours/routes/viewRoutes.js
+++ 4-natours/routes/viewRoutes.js
@@ -4,8 +4,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(authController.isLoggedIn);
+
 router.get('/', viewController.getOverView);
-router.get('/tour/:slug', authController.protect, viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
