1d2aeb276e04e6854ae58fe90e4e824e5fe74178
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2020-12-30 13:18:29 +0630

    Including a Map With Mapbox - Part 1

--- 4-natours/controllers/viewController.js
+++ 4-natours/controllers/viewController.js
@@ -22,10 +22,8 @@ exports.getTour = catchAsync(async (req, res) => {
     fields: 'review rating user',
   });
 
-  console.log(tour);
-
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
     tour,
   });
 });
