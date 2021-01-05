9d50ca625ff20db574f8b9f0823aa76c2b06282d
Ye Kyaw Swar Aung <yekyawswaraung@gmail.com>
2021-01-05 11:39:29 +0630

    Using Sendgrid for "Real" Emails

--- 4-natours/utils/email.js
+++ 4-natours/utils/email.js
@@ -7,13 +7,19 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
+    this.from = `Ye Kyaw Swar Aung <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
